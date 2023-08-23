fa17198fe04aa94500d34b6c96dbc767d8998d96
Ephemerian <106543055+Ephemerian@users.noreply.github.com>
2023-08-24 01:54:17 +0300

    update

---
+++ js/task-08.js
@@ -0,0 +1,15 @@
+const submitFormEL = document.querySelector(".login-form");
+submitFormEL.addEventListener("submit", onSubmit);
+function onSubmit(evt) {
+  evt.preventDefault();
+  const { email, password } = evt.currentTarget.elements;
+  if (email.value.trim() === "" || password.value.trim() === "") {
+    return alert("All fields must be filled in");
+  }
+  const userData = {
+    name: email.value,
+    password: password.value,
+  };
+  console.log(userData);
+  evt.target.reset();
+}
